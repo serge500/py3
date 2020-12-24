1653853381fdddcc4a41e9b411a11437e66915af
Andrey Tsvetkov <a.tsvetkov@rt-solar.ru>
2020-12-24 10:13:54 +0300

    test

--- PYTHON_CRYPTO_BAD_IV.py
+++
@@ -1,6 +0,0 @@
-from Crypto.Cipher import AES
-from Crypto import Random
-
-key = Random.new().read(AES.block_size)
-# <yes> <report> PYTHON_CRYPTO_BAD_IV jjjykp
-cipher = AESCipher(key, MODE_CBC, IV=key)
