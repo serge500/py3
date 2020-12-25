59030b7bd337ad4697a21e8e13aad225b5fe740a
Andrey Tsvetkov <a.tsvetkov@rt-solar.ru>
2020-12-25 22:30:26 +0300

    test

--- PYTHON_CRYPTO_IV_NULL.py
+++
@@ -1,6 +0,0 @@
-from Crypto.Cipher import AES
-from Crypto import Random
-
-key = Random.new().read(AES.block_size)
-# <yes> <report> PYTHON_CRYPTO_IV_NULL hmmmk6
-cipher = AES.new(key, AES.MODE_CBC, IV=None)
