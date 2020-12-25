6215cd85bf72b813da197b3caa938de5ce818ccf
Andrey Tsvetkov <a.tsvetkov@rt-solar.ru>
2020-12-25 22:31:11 +0300

    test

--- PYTHON_CRYPTO_IV_HARDCODED.py
+++
@@ -1,8 +0,0 @@
-from Crypto.Cipher import AES
-from Crypto import Random
-
-key = Random.new().read(AES.block_size)
-# <yes> <report> PYTHON_CRYPTO_IV_HARDCODED hj6kk6
-cipher = AES.new(key, AES.MODE_CBC, IV="key")
-# <yes> <report> PYTHON_CRYPTO_IV_HARDCODED hrtyk6
-cipher = AESCipher(key, MODE_CBC, IV="key")
